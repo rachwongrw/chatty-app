66e9ee6ed0c2fc65b52c85954ac4ece137b6a0fc
Rachel Wong <rwrachelwong@gmail.com>
2018-11-28 22:26:56 +0000

    Send messages to server

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import NavBar from './Navbar.jsx';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -22,20 +24,36 @@ class App extends Component {
       ]
     }
     this.addChatMsg = this.addChatMsg.bind(this);
+    // this.socket = { socket }
   }
   addChatMsg(message) {
     let messageItem = {
       id: (this.state.messages.length + 1),
       username: 'Bob',
-      content: message
+      content: message.content
     };
-    const oldMessageItems = this.state.messages;
-    const newMessageItems = [...oldMessageItems, messageItem];
-    this.setState({messages: newMessageItems});
+    // const oldMessageItems = this.state.messages;
+    // const newMessageItems = [...oldMessageItems, messageItem];
+    this.socket.send(JSON.stringify(messageItem));
+    // this.setState({messages: newMessageItems});
   }
 
   componentDidMount() {
     console.log('componentDidMount <App />');
+    this.socket = new WebSocket('ws://0.0.0.0:3001');
+
+    this.socket.onopen = function(event) {
+      console.log('Connected to Server');
+    }
+
+    this.socket.onerror = function(error) {
+      console.log('WebSocket Error: ' + error);
+    };
+
+    this.socket.addEventListener('message', (message) => {
+      console.log('HEY A NEW MESSAGE FROM THE SERVER', message)
+    });
+
     setTimeout(() => {
       console.log('Simulating incoming message');
       const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
@@ -48,7 +66,7 @@ class App extends Component {
       <div>
         <NavBar />
         <MessageList messages = {this.state.messages}/>
-        <ChatBar user = {this.state} addChatMsg = {this.addChatMsg} />
+        <ChatBar user = {this.state} addChatMsg={this.addChatMsg} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,48 +1,49 @@
 import React, { Component } from 'react';
 
-function ChatBar({ user, addChatMsg }) {
-  const _handleKeyPress = event => {
+class ChatBar extends Component {
+  _handleKeyPress = event => {
     if (event.key === 'Enter') {
-      console.log('props', {user});
-      console.log('addChatMsg', {addChatMsg});
-      console.log('what is the user name:', user.currentUser.name);
-      console.log('what are is the target value!?!', event.target.value);
-      const messageInput = event.target.value;
-      addChatMsg(messageInput);
-      event.target.value = '';
+      console.log('do validate');
+      console.log('props', this.props);
+      console.log('what is the user name:', this.props.user.currentUser.name);
+      console.log('what are the target elements!!!', event.target.value);
+      let message = {
+        id: 5,
+        username: this.props.user.currentUser.name,
+        content: event.target.value
+      };
+      this.props.addChatMsg(message);
+      message.value = '';
     }
   }
-  return (   
-    <footer className='chatbar'>
-      <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={user.currentUser.name}/>
-      <input className='chatbar-message' type= 'text' placeholder='Type a message and hit ENTER' defaultValue='' name='message' onKeyPress= { _handleKeyPress }/>
-    </footer>
-  );
+  render() {
+    return (   
+      <footer className='chatbar'>
+        <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue=''/>
+        <input className='chatbar-message' type= 'text' placeholder='Type a message and hit ENTER' defaultValue='' name='message' onKeyPress= { this._handleKeyPress }/>
+      </footer>
+      )
+  }
 }
 
 export default ChatBar;
 
-
-// OLD CODE
-
-// class ChatBar extends Component {
-//   _handleKeyPress = event => {
+// function ChatBar({ user, onMessageSend }) {
+//   const _handleKeyPress = event => {
 //     if (event.key === 'Enter') {
-//       console.log('do validate');
-//       console.log('props', this.props);
-//       console.log('what is the user name:', this.props.user.currentUser.name);
-//       console.log('what are the target elements!!!', event.target.elements);
-//       const messageInput = event.target.elements.message;
-//       this.props.addChatMsg(messageInput.value);
-//       messageInput.value = '';
+//       console.log('props', {user});
+//       console.log('what is the props object:', user.messages);
+//       console.log('what is the target?', event.target);
+//       console.log('what is the target value!?!', event.target.value);
+//       const messageInput = event.target.value;
+//       onMessageSend(messageInput);
+//       event.target.value = '';
 //     }
 //   }
-//   render() {
-//     return (   
-//       <footer className='chatbar'>
-//         <input className='chatbar-username' placeholder='Your Name (Optional)' defaultValue={this.props.user.currentUser.name}/>
-//         <input className='chatbar-message' type= 'text' placeholder='Type a message and hit ENTER' defaultValue='' name='message' onKeyPress= { this._handleKeyPress }/>
-//       </footer>
-//       )
-//   }
+//   return (   
+//     <footer className='chatbar' onKeyPress= { _handleKeyPress }>
+//       <input className='chatbar-username' type='text' placeholder='Your Name (Optional)' defaultValue='' name='username'/>
+//       <input className='chatbar-message' type='text' placeholder='Type a message and hit ENTER' defaultValue='' onKeyUp= {this.props.aname='message'/>
+//     </footer>
+//   );
 // }
