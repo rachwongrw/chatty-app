372eaaa9da648f15e88155b040248c56ddb42c85
Rachel Wong <rwrachelwong@gmail.com>
2018-11-29 03:51:53 +0000

    code refactor, added notes below in chatbar.jsx

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -10,10 +10,10 @@ class ChatBar extends Component {
     this.handleNameChange = this.handleNameChange.bind(this);;
   }
 
-  _handleKeyPress = event => { // want the msg to have handlekeypress to send the content to server
+  _handleKeyPress = event => {
     if (event.key === 'Enter') {
       let message = {
-        id: '',
+        id: '', // id to be assigned once it's sent to the server
         username: this.state.currentUser.name,
         content: event.target.value
       };
@@ -23,7 +23,6 @@ class ChatBar extends Component {
   }
 
   handleNameChange (event) {
-    console.log('name target value', event.target.value );
     this.setState({ 
       currentUser: {name: event.target.value } 
     });
@@ -32,10 +31,17 @@ class ChatBar extends Component {
     return (   
       <footer className='chatbar'>
         <input className='chatbar-username' type= 'text' placeholder='Your Name (Optional)' defaultValue={this.props.username} name='name' onChange= {this.handleNameChange} />
-        <input className='chatbar-message' type= 'text' placeholder='Type a message and hit ENTER' defaultValue={this.state.messages} name='messages' onKeyPress= { this._handleKeyPress }/>
+        <input className='chatbar-message' type= 'text' placeholder='Type a message and hit ENTER' defaultValue='' name='messages' onKeyPress= { this._handleKeyPress }/>
       </footer>
     )
   }
 }
 
 export default ChatBar;
+
+
+/* 
+
+defaultValues are needed in the input field. This will hold the value until it gets changed. for the name, we've set it to props.username.
+handleKeyPress is for when we want to send the message provided in the input to the server along with the 'set' username. 
+*/
