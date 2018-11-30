bf86b9c04231b41a2de10a06c13314d1e4c3d474
Rachel Wong <rwrachelwong@gmail.com>
2018-11-30 21:28:05 +0000

    Add Avo emoji in navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ class NavBar extends Component {
   render() {
   return (
     <nav className='navbar'>
-      <a href='/' className='navbar-brand'>Holy Guac <small>Chat Room</small></a>
+      <a href='/' className='navbar-brand'>Holy Guac 🥑 <small>Chat Room</small></a>
       <span className='users'>
         {this.props.numberOfUsers} users online
       </span>
